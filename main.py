9b5a608672c1bcdcc081a6e60703d5cf6c8674f7
Nipitpon Kampolrat <6410685173@cn330>
2023-10-19 14:49:51 +0700

    Refactor code to read data from csv

--- main.py
+++ main.py
@@ -1,22 +1,11 @@
-import csv
-
 # For tutorial on FastAPI
 # See https://fastapi.tiangolo.com/tutorial/first-steps/
 from fastapi import FastAPI
 
-# get country data from csv file
-filename = "world_table_country.csv"
-with open(filename, "r", encoding="utf8") as csv_file:
-    csv_reader = csv.reader(csv_file)
-    headers = next(csv_reader)
-    data_country = [{k: v for (k, v) in zip(headers, row)} for row in csv_reader]
+from utils import get_data_from_csv
 
-# get city data from csv file
-filename = "world_table_city.csv"
-with open(filename, "r", encoding="utf8") as csv_file:
-    csv_reader = csv.reader(csv_file)
-    headers = next(csv_reader)
-    data_city = [{k: v for (k, v) in zip(headers, row)} for row in csv_reader]
+data_country = get_data_from_csv("world_table_country.csv")
+data_city = get_data_from_csv("world_table_city.csv")
 
 # create an instance of class FastAPI named "app"
 app = FastAPI()
@@ -33,6 +22,14 @@ async def read_root() -> dict:
 async def read_countries() -> dict:
     return {"result": data_country}
 
+# define function that handles "GET" request with endpoint "/world/country/{name}"
+# "/world/country/{name}" is a "path paramter" endpoint
+@app.get("/world/country/{name}")
+async def read_country(name: str) -> dict:
+    for row in data_country:
+        if row["Name"].lower() == name.lower():
+            return {"result": row}
+    return {"result": {}}
 
 # define function that handles "GET" request with endpoint "/world/city/{name}"
 # "/world/city/{name}" is a "path paramter" endpoint
